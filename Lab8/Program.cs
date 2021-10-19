f6e2ca81e79f923f54026bf090f172df0affac1e
Professional <Professional@USER-PC>
2021-10-19 23:47:54 +0300

    Добавьте файлы проекта.

---
+++ Lab8/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lab8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] dirs = Directory.GetDirectories(@"E:\Учеба ВлГУ");
+            foreach (var dir in dirs)
+            {
+                Console.WriteLine("Наименование каталога: {0}",dir);
+                Console.WriteLine("Список файлов:");
+                string[] secondDirs = Directory.GetFiles(dir);
+                for (int i = 0; i < secondDirs.Length; i++)
+                {
+                    Console.WriteLine(secondDirs[i]);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "C:/Users/Professional/Desktop/ИТМО_учеба/Lab8/Lab8.txt";// Создаем текстовый файл в каталоге текущей лабораторной работы
+            using (File.Create(path));
+            Random random = new Random();
+            StreamWriter sw = new StreamWriter(path, true);
+            {
+                for (int i = 0; i < 10; i++)//Записываем построчно в текстовый файл 10 случайных чисел от -50 до +50
+                {
+                    int a = random.Next(-50, 50);
+                    sw.WriteLine("{0}",a);
+                }
+            }
+            sw.Close();
+            int sum =0;
+            StreamReader sr = new StreamReader(path);
+            string line;
+            while ((line = sr.ReadLine()) != null)//Считываем построчно числа из файладо тех пор пока не "упремся" в пустую строку и накапливаем сумму
+                {
+                    Console.WriteLine(line);
+                    sum += Convert.ToInt32(line);
+                }
+            Console.WriteLine();
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
